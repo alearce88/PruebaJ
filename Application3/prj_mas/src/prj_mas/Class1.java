d2ac82980368685fca768b6e4db1ef3909b523d6
alearce88 <alearce88@gmail.com>
2015-10-12 18:51:33 -0600

    Nuevo método
    Método Despedida

--- Application3/prj_mas/src/prj_mas/Class1.java
+++ Application3/prj_mas/src/prj_mas/Class1.java
@@ -9,6 +9,11 @@ public class Class1 {
         
         return "Hola";
     }
+    
+    public String despedida (){
+        
+        return "Adiós";
+    }
 
     public static void main(String[] args) {
         Class1 class1 = new Class1();
