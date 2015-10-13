f3e3c830de5e3b4b9906ff8b884ca5883a9f3528
alearce88 <alearce88@gmail.com>
2015-10-12 18:35:31 -0600

    Aplicación 1
    Primera aplicación

---
+++ Application3/prj_mas/src/prj_mas/Class1.java
@@ -0,0 +1,16 @@
+package prj_mas;
+
+public class Class1 {
+    public Class1() {
+        super();
+    }
+    
+    public String saludo (){
+        
+        return "Hola";
+    }
+
+    public static void main(String[] args) {
+        Class1 class1 = new Class1();
+    }
+}
